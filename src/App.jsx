b6610b28c81c07cb7038f54875187bed34842e74
unknown <mrnafsu07@gmail.com>
2025-04-12 22:24:04 +0600

    added services section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
 
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
       <Hero />
+      <Services />
     </main>
   );
 };
